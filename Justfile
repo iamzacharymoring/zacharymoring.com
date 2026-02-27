969f2fe6449690e4c72e215143d7446b03a4cd84
zpm <iamzacharymoring@gmail.com>
2026-02-26 21:10:01 -0600

    Add articles homepage section and page for listing articles
    Also, add a Justfile task for making new articles. For now, all an
    article needs is a title, so it's pretty straightforward. Unfortunate
    that the expansion-wrangling to get the file name is so complicated...
    but at least it's kind of charming?
    
    Signed-off-by: zpm <iamzacharymoring@gmail.com>

--- Justfile
+++ Justfile
@@ -3,3 +3,26 @@ serve:
 
 build:
 	@npx @11ty/eleventy
+
+make $kind $title:
+	#!/usr/bin/env bash
+
+	case $kind in
+		"article")
+			fname=${title//[\"\'\`]/}		# Delete all quotes
+			fname=${fname//[^A-Za-z0-9]/-}	# Replace nonalphanumeric with -
+			fname=${fname,,}				# Force lowercase
+
+			cat > ./src/articles/${fname}.md <<- EOF
+			---
+			title: $title
+			---
+
+			# $title
+			EOF
+			;;
+
+		*)
+			exit 1;
+			;;
+	esac
--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,14 +1,17 @@
 import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import markdownIt from "markdown-it";
 
-export default async function (eleventyConfig) {
-  // Globally set a default template
-  eleventyConfig.addGlobalData("layout", "page.njk");
+// =============================================================================
+// Defs
+// =============================================================================
 
-  // Plugins
+function configureGlobalSettings(eleventyConfig) {
+  eleventyConfig.addGlobalData("layout", "page.njk");
+}
+
+function configureMarkdownExtensions(eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
 
-  // Customize markdown rendering
   let md = new markdownIt();
   const defaultRender = md.renderer.rules.fence;
   md.renderer.rules.fence = (tokens, idx, options, env, slf) => {
@@ -16,7 +19,36 @@ export default async function (eleventyConfig) {
     html = `<div class="box">${html}</div>`;
     return html;
   };
+
   eleventyConfig.setLibrary("md", md);
+}
+
+function createCustomFilters(eleventyConfig) {
+  eleventyConfig.addNunjucksFilter("startswith", (s, p) => s.startsWith(p));
+  eleventyConfig.addNunjucksFilter("take", (arr, n) => arr.slice(0, n));
+  eleventyConfig.addNunjucksFilter(
+    "yyyymmdd",
+    (d) => d.toISOString().split("T")[0],
+  );
+}
+
+function createCustomCollections(eleventyConfig) {
+  eleventyConfig.addCollection("articles", (collectionsApi) =>
+    collectionsApi
+      .getFilteredByGlob("./src/articles/*.md")
+      .sort((a, b) => b.date - a.date),
+  );
+}
+
+// =============================================================================
+// Exports
+// =============================================================================
+
+export default async function (eleventyConfig) {
+  configureGlobalSettings(eleventyConfig);
+  configureMarkdownExtensions(eleventyConfig);
+  createCustomFilters(eleventyConfig);
+  createCustomCollections(eleventyConfig);
 }
 
 export const config = {
